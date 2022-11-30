1a5230b712db18400ebf54de7e8ba51623c95570
pintoflager <jaakko0606@gmail.com>
2022-11-30 18:30:42 -0500

    provider to provisioner with nodes

--- node/create.sh
+++ node/create.sh
@@ -53,3 +53,82 @@ carburator provisioner request \
     --provisioner "$provisioner" || exit 120
 
 carburator print terminal success "Hetzner node(s) created."
+
+
+###
+# Private networking
+#
+
+# Check if our nodes should be connected
+provisioner_net="$PROJECT_PATH/provisioners/$provisioner/providers/$PROVIDER_NAME/network"
+
+# How many nodes are we dealing with
+len=$(carburator get json nodes array --path "$provider_node/.exec.json" | wc -l)
+
+# Hetzner has 2 network zones, europe and yankie east
+declare -a eu_nodes; declare -a us_nodes;
+
+for (( i=0; i<len; i++ )); do
+    # Only take nodes that have their private net boolean true.
+    if carburator get json "nodes.$i.connectivity.private_net" boolean \
+    --path "$provider_node/.exec.json"
+    then
+        name=$(carburator get json "nodes.$i.hostname" boolean \
+            --path "$provider_node/.exec.json")
+
+        location=$(carburator get json "nodes.$i.location.name" boolean \
+            --path "$provider_node/.exec.json")
+
+        if [[ $location == "ash" ]]; then us_nodes+=("$name")
+        else eu_nodes+=("$name"); fi
+    fi
+done
+
+# Network name is the same as the node group name
+net_name=$(carburator get env NODE_GROUP --path "$provider_node/.exec.env")
+
+# 254 nodes is enough..?
+range="10.10.0.0/24"
+
+# Each net zone should be called individually and only if they hold nodes.
+# At least following should be provided
+if [[ ${#eu_nodes[@]} -gt 0 ]]; then
+    carburator put json network "{\
+    name: \"$net_name-eu\",\
+    range: \"$range\",\
+    zone: eu-central,\
+    type: cloud}" --path "$provisioner_net/.eu.nodes.json"
+
+    IFS=\, eval 'node_csv="${eu_nodes[*]}"'
+    
+    # shellcheck disable=SC2154
+    carburator put json nodes "[$node_csv]" \
+        --path "$provisioner_net/.eu.nodes.json"
+
+# Make sure files from previous iterations are not present.
+else
+    rm -f "$provisioner_net/.eu.nodes.json"
+fi
+
+if [[ ${#us_nodes[@]} -gt 0 ]]; then
+    carburator put json network "{\
+    name: \"$net_name-us\",\
+    range: \"$range\",\
+    zone: us-east,\
+    type: cloud}" --path "$provisioner_net/.us.nodes.json"
+
+    IFS=\, eval 'node_csv="${us_nodes[*]}"'
+    carburator put json nodes "[$node_csv]" \
+        --path "$provisioner_net/.us.nodes.json"
+
+# Make sure files from previous iterations are not present.
+else
+    rm -f "$provisioner_net/.us.nodes.json"
+fi
+
+# Invoke the provisioner
+carburator provisioner request \
+    create \
+    network \
+    --provider "$PROVIDER_NAME" \
+    --provisioner "$provisioner" || exit 120
